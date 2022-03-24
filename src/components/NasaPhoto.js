70d7178892cc904f5eb9822c175ccdce6cdeebcf
Taylor <Taylor88stacey@gmail.com>
2022-03-24 14:01:08 -0700

    some styled components, and some css

--- src/components/NasaPhoto.js
+++ src/components/NasaPhoto.js
@@ -1,21 +1,44 @@
 import React from "react";
+import styled, { keyframes } from 'styled-components';
 
-const dummyData = {
-    date: "2021-12-23",
-    explanation: "still looking",
-    hdurl: "https://apod.nasa.gov/apod/image/2112/20211221Tezel.jpg",
-    title: "Three Planets and a Comet"
+// const dummyData = {
+//     date: "2021-12-23",
+//     explanation: "still looking",
+//     hdurl: "https://apod.nasa.gov/apod/image/2112/20211221Tezel.jpg",
+//     title: "Three Planets and a Comet"
+// }
+
+const kf = keyframes`
+70% {
+    opacity: .7;
 }
+100% {
+    opacity: 1;
+    transform: scale(1) rotateZ(0)
+}
+`
+const StyledDetails =styled.div`
+transform: scale(5) rotateZ(360deg);
+  animation: ${kf} 0.5s ease-in-out forwards;
+  background-color: grey;
+ 
+   
+`
+// .explanation`
+// &:hover .explanation {
+//       background-color: lemonchiffon;
+//       padding: 1.4rem;
+//   }
+// `
 
 const NasaPhoto = (props) => {
     return (
-        <div className="nasa-photo-wrapper">
+        <StyledDetails className="nasa-photo-wrapper">
             <h3>{props.photo.title}</h3>
             <p>{props.photo.date}</p>
-            <img src={props.photo.hdurl}/>
+            <img src={props.photo.hdurl} />
             <p className="explanation">{props.photo.explanation}</p>
-
-        </div>
+        </StyledDetails>
     )
 }
 
